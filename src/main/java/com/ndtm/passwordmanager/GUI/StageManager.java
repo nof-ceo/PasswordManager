a431b13eb6385dcd69b209e31febc2b0b95f16de
neydtm <degtyarevn5@gmail.com>
2023-07-25 23:20:50 +0400

    feat: manage current stage

---
+++ src/main/java/com/ndtm/passwordmanager/GUI/StageManager.java
@@ -0,0 +1,38 @@
+package com.ndtm.passwordmanager.GUI;
+
+import com.ndtm.passwordmanager.HelloApplication;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public abstract class StageManager extends Stage {
+    public static Stage currentStage;
+
+    public static void openAuthGui() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("authPasswordManager.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 600);
+        scene.setFill(Color.TRANSPARENT);
+        settingStage(scene);
+    }
+
+
+    public static void openPasswordManagerGui() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("passwordManagerMain.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+        scene.setFill(Color.TRANSPARENT);
+        settingStage(scene);
+    }
+
+    private static void settingStage(Scene scene) throws IOException{
+        Stage stage = new Stage();
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setScene(scene);
+        stage.show();
+        currentStage = stage;
+    }
+
+}
