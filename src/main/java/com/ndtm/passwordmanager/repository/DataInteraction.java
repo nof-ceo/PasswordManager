eb6e1658be81f0d7d1ac4b57bf2fe6bcf8d856f6
neydtm <degtyarevn5@gmail.com>
2023-07-28 21:50:35 +0400

    feat: add @Repository

--- src/main/java/com/ndtm/passwordmanager/repository/DataInteraction.java
+++ src/main/java/com/ndtm/passwordmanager/repository/DataInteraction.java
@@ -3,9 +3,12 @@ package com.ndtm.passwordmanager.repository;
 import com.ndtm.passwordmanager.manage.WebSite;
 import com.ndtm.passwordmanager.userActions.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 
 import java.util.Optional;
 
+@Repository
 public interface DataInteraction extends JpaRepository {
 
     // заменить когда будет extends JpaRepo
