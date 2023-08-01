57ae83dab2371b75487327b1c65bf2adfe1f4c50
neydtm <degtyarevn5@gmail.com>
2023-08-02 00:17:22 +0400

    feat: action for login and register label

--- src/main/java/com/ndtm/passwordmanager/controller/AuthController.java
+++ src/main/java/com/ndtm/passwordmanager/controller/AuthController.java
@@ -3,11 +3,17 @@ package com.ndtm.passwordmanager.controller;
 import com.ndtm.passwordmanager.GUI.AuthGui;
 import com.ndtm.passwordmanager.GUI.StageManager;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+
 import java.io.IOException;
 
 public class AuthController {
 
+    public Label buttonForChoseLogin;
+    public Label buttonForChoseRegister;
+    public Rectangle uselessDesignElement;
     private double mousePressedX;
     private double mousePressedY;
 
@@ -30,4 +36,27 @@ public class AuthController {
         mousePressedX = event.getX();
         mousePressedY = event.getY();
     }
+
+    @FXML
+    public void loginChosen() {
+        buttonForChoseLogin.setStyle("-fx-text-fill: #c1b3ff");
+        buttonForChoseRegister.setStyle("-fx-text-fill: #c3c3c3");
+        uselessDesignElement.setLayoutX(68);
+        uselessDesignElement.setWidth(75);
+
+        AuthGui.hideRegisterMenu();
+        AuthGui.showLoginMenu();
+    }
+
+    @FXML
+    public void registerChosen() {
+        buttonForChoseLogin.setStyle("-fx-text-fill: #c3c3c3");
+        buttonForChoseRegister.setStyle("-fx-text-fill: #c1b3ff");
+        uselessDesignElement.setLayoutX(230);
+        uselessDesignElement.setWidth(120);
+
+        AuthGui.hideLoginMenu();
+        AuthGui.showRegisterMenu();
+    }
+
 }
