8a59a38e10e3f87d83161f6f635b4f0aba105c20
neydtm <degtyarevn5@gmail.com>
2023-08-26 06:38:06 +0400

    feat: interaction with data entered by the user

---
+++ src/main/java/com/ndtm/passwordmanager/manage/SavedDataService.java
@@ -0,0 +1,39 @@
+package com.ndtm.passwordmanager.manage;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.ndtm.passwordmanager.repository.SavedDataInteraction;
+import com.ndtm.passwordmanager.userActions.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/** TODO:
+ * 1. пароль будет закодирован в AES, т.к хешированные данные нельзя вернуть в прежнюю форму
+ */
+
+@Service
+public class SavedDataService {
+
+    @Autowired
+    SavedDataInteraction savedDataInteraction;
+
+    public SavedData saveData(String siteTitle, String siteUrl, String login, String password, byte[] creditCard, byte[] expiredDate, byte[] cvv, byte[] phoneNumber) {
+        String passwordHash = BCrypt.withDefaults().hashToString(12, password.toCharArray());
+
+        SavedData savedData = new SavedData(siteTitle, siteUrl, login, passwordHash, creditCard, expiredDate, cvv, phoneNumber);
+        savedData.setUserId(UserService.currentUser.getId());
+
+        return savedDataInteraction.save(savedData);
+    }
+
+    public SavedData saveData(SavedData savedData) {
+        savedData.setUserId(UserService.currentUser.getId());
+
+        return savedDataInteraction.save(savedData);
+    }
+
+    public Optional<SavedData> findByLogin(String login) {
+        return savedDataInteraction.findByLogin(login);
+    }
+}
