81599979f15f4a77056c019fd344ecc8048f97ae
neydtm <degtyarevn5@gmail.com>
2023-09-07 00:17:34 +0400

    small changes

--- src/main/java/com/ndtm/passwordmanager/PasswordManagerApplication.java
+++ src/main/java/com/ndtm/passwordmanager/PasswordManagerApplication.java
@@ -1,7 +1,6 @@
 package com.ndtm.passwordmanager;
 
 import com.ndtm.passwordmanager.GUI.StageManager;
-import com.ndtm.passwordmanager.controller.AuthMenuController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,22 +8,21 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.IOException;
 
-// gui shows after spring boot has ready
 public class PasswordManagerApplication extends Application {
 
     private ConfigurableApplicationContext applicationContext;
 
 	@Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException{
         applicationContext = new SpringApplicationBuilder(StockUiApp.class).run();
 
         applicationContext.publishEvent(new StageReadyEvent(stage));
     }
 
-    private class StageReadyEvent {
+    private class StageReadyEvent extends StageManager{
         public StageReadyEvent(Stage stage) throws IOException {
             StageManager.currentStage = stage;
-            StageManager.openAuthGui();
+            openAuthGui();
         }
     }
 }
