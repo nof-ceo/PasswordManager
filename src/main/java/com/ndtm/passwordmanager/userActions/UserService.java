a9fcb718dfa2710ef20adc6d6b7446b93db316a1
neydtm <degtyarevn5@gmail.com>
2023-09-17 14:26:46 +0400

    feat: sending a message to user email

--- src/main/java/com/ndtm/passwordmanager/userActions/UserService.java
+++ src/main/java/com/ndtm/passwordmanager/userActions/UserService.java
@@ -3,21 +3,26 @@ package com.ndtm.passwordmanager.userActions;
 import at.favre.lib.crypto.bcrypt.BCrypt;
 import com.ndtm.passwordmanager.repository.UserDataInteraction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import javax.mail.internet.MimeMessage;
 import java.util.Optional;
+import java.util.UUID;
 
 /** TODO:
- * 1. проверять верефицирован ли email в makeRegister
- * 2. можно заменить библиотеку bcrypt, на bcrypt в spring security
  */
 
 @Service
 public class UserService {
 
     @Autowired
-    UserDataInteraction userDataInteraction;
+    private UserDataInteraction userDataInteraction;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     private static final String EMAIL_PATTERN =
             "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
@@ -27,12 +32,17 @@ public class UserService {
 
     static UserDataInteraction staticUserDataInteraction;
 
+    // мб есть вариант без static
+    static JavaMailSender staticJavaMailSender;
+
     @PostConstruct
     public void init() {
         UserService.staticUserDataInteraction = userDataInteraction;
+        UserService.staticJavaMailSender = javaMailSender;
     }
 
-    public static User currentUser;
+    public static User currentActiveUser;
+    public static User currentVerificationUser;
 
     public User makeRegister(String firstName, String lastName, String login, char[] password, String email) {
         String passwordHash = BCrypt.withDefaults().hashToString(12, password);
@@ -41,6 +51,9 @@ public class UserService {
     }
 
     public User makeRegister(User user) {
+        String passwordHash = BCrypt.withDefaults().hashToString(12, user.getPassword().toCharArray());
+        user.setPassword(passwordHash);
+
         return staticUserDataInteraction.save(user);
     }
 
@@ -53,17 +66,38 @@ public class UserService {
     }
 
     public boolean makeAuth(String inputLogin, char[] inputPassword) {
-
         Optional<User> foundedUser = staticUserDataInteraction.findByLogin(inputLogin);
 
         if(foundedUser.isPresent()) {
             boolean result = BCrypt.verifyer().verify(inputPassword, foundedUser.get().getPassword()).verified;
             if(result)
-                currentUser = foundedUser.get();
+                currentActiveUser = foundedUser.get();
             return result;
         } else {
             return false;
         }
+    }
+
+    public void sendVerificationLink(User user) throws Exception{
+        currentVerificationUser = user;
+
+        String verificationToken = UUID.randomUUID().toString();
+
+        currentVerificationUser.setVerificationToken(verificationToken);
+
+        // in future change to real site
+        String verifyLink = "localhost:8080/verify?verificationToken=" + verificationToken;
+
+        MimeMessage mimeMessage = staticJavaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+
+        helper.setTo(user.getEmail());
+        helper.setSubject("Verification");
+        helper.setText("<br>" +
+                "Please click the link below to verify your registration:<br>" +
+                "<h3><a href=" + verifyLink +  " target=\"_self\">VERIFY</a></h3>", true);
+
+        staticJavaMailSender.send(mimeMessage);
     }
 
     public boolean checkEmailFieldForCorrectAndNotNull(String email) {
