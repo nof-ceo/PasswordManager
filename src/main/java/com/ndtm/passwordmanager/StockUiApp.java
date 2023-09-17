5a09df2d915d229d4190e31fd8ccd356896ad71b
neydtm <degtyarevn5@gmail.com>
2023-09-17 11:36:50 +0400

    .

--- src/main/java/com/ndtm/passwordmanager/StockUiApp.java
+++ src/main/java/com/ndtm/passwordmanager/StockUiApp.java
@@ -2,8 +2,9 @@ package com.ndtm.passwordmanager;
 
 import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class StockUiApp {
     public static void main(String[] args) {
         Application.launch(PasswordManagerApplication.class, args);
