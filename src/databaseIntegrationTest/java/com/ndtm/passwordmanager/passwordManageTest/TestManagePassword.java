79142eeb290554c3876d45d010b3c45cc89e436f
neydtm <degtyarevn5@gmail.com>
2023-10-29 21:26:23 +0400

    feat: encrypting

--- src/databaseIntegrationTest/java/com/ndtm/passwordmanager/passwordManageTest/TestManagePassword.java
+++ src/databaseIntegrationTest/java/com/ndtm/passwordmanager/passwordManageTest/TestManagePassword.java
@@ -1,18 +1,13 @@
 package com.ndtm.passwordmanager.passwordManageTest;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
-import com.ndtm.passwordmanager.configurations.TestMailConfiguration;
-import com.ndtm.passwordmanager.manage.SavedData;
-import com.ndtm.passwordmanager.manage.SavedDataService;
-import com.ndtm.passwordmanager.userActions.User;
-import com.ndtm.passwordmanager.userActions.UserService;
+import com.ndtm.passwordmanager.manage.*;
+import com.ndtm.passwordmanager.userActions.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -25,7 +20,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @ComponentScan(basePackages = { "com.ndtm.passwordmanager.*" })
-@Import(TestMailConfiguration.class)
 public class TestManagePassword {
 
     @Autowired
@@ -38,23 +32,34 @@ public class TestManagePassword {
 
     User user = new User("Dree", "Doe", "testLoginUser", "testPasswordUser", "testEmail@gmail.com");
 
-    SavedData savedData = new SavedData("vk.com", "https://vk.com", login, BCrypt.withDefaults().hashToString(12, "testPassword".toCharArray()), null, null, null, null);
+    SavedData savedData;
 
     @Test
     public void testSavePassword() {
         userService.makeRegister(user);
         userService.makeAuth("testLoginUser", "testPasswordUser".toCharArray());
 
-        assertEquals(savedData, savedDataService.saveData(savedData));
+        savedData = new SavedData("vk.com", "https://vk.com", login, BCrypt.withDefaults().hashToString(12, "testPassword".toCharArray()), null, null, null, null);
+
+        SavedData encryptedData = savedDataService.encryptData(savedData);
+
+        assertEquals(encryptedData, savedDataService.saveData(encryptedData));
     }
 
     @Test
     public void testFoundData() {
         userService.makeRegister(user);
         userService.makeAuth("testLoginUser", "testPasswordUser".toCharArray());
-        savedDataService.saveData(savedData);
 
-        assertEquals(savedData, savedDataService.findByLogin(login).get());
+        savedData = new SavedData("vk.com", "https://vk.com", login, BCrypt.withDefaults().hashToString(12, "testPassword".toCharArray()), null, null, null, null);
+
+        SavedData encryptedData = savedDataService.encryptData(savedData);
+
+        savedDataService.saveData(encryptedData);
+        SavedData foundedData = savedDataService.saveData(encryptedData);
+
+        assertEquals(encryptedData, savedDataService.findByLogin(encryptedData.getLogin()).get());
+        assertEquals(encryptedData, savedDataService.findById(encryptedData.getId()).get());
     }
 
 
